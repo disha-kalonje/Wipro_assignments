9b4f8d49f6031ca6a0d0d398d113115e8e09d9fe
Disha Kalonje <dishavinodkalonje6148@gmail.com>
2024-06-01 13:05:50 +0530

    SQL_Assignment_4.sql

--- Oracle_sql/SQL_Assignment_4.sql
+++ Oracle_sql/SQL_Assignment_4.sql
@@ -1,1 +1,9 @@
+-- Assignment : Write SQL statements to CREATE a new database and tables that reflect the library schema you designed earlier. Use ALTER statements to modify the table structures and DROP statements to remove a redundant table.
 
+-- Modify Table Structure (ALTER)
+ALTER TABLE books
+  ADD genre VARCHAR2(50);
+
+-- Drop a Table (DROP)
+-- Let's say you decide the Loans table is redundant and want to remove it :
+  DROP TABLE loans;
