2dca9d4cd9d8f0053633f87d764a263bf71c7fa8
Disha Kalonje <dishavinodkalonje6148@gmail.com>
2024-06-01 13:07:14 +0530

    SQL_Assignment_3.sql

--- Oracle_sql/SQL_Assignment_3.sql
+++ Oracle_sql/SQL_Assignment_3.sql
@@ -1,9 +1,10 @@
 -- Assignment : Design a database schema for a library system, including tables, fields, and constraints like NOT NULL, UNIQUE, and CHECK. Include primary and foreign keys to establish relationships between tables.
 
 -- Create the Database 
-CREATE DATABASE library_db
+CREATE DATABASE library_db;
 
--- Create Tables (Library Schema) :
+-- Create Tables (Library Schema)
+
 -- Books table
 CREATE TABLE books (
   book_id NUMBER PRIMARY KEY,
@@ -20,14 +21,14 @@ CREATE TABLE authors (
   last_name VARCHAR2(100) NOT NULL
 );
 
--- Borrowers table (Assuming you want to track borrowers)
+-- Borrowers table (To track borrowers)
 CREATE TABLE borrowers (
   borrower_id NUMBER PRIMARY KEY,
   name VARCHAR2(255) NOT NULL,
   phone_number VARCHAR2(20)
 );
 
--- Loans table (Assuming you want to track loans)
+-- Loans table (To track loans)
 CREATE TABLE loans (
   loan_id NUMBER PRIMARY KEY,
   book_id NUMBER REFERENCES books(book_id) ON DELETE SET NULL,
@@ -35,11 +36,3 @@ CREATE TABLE loans (
   loan_date DATE,
   return_date DATE
 );
-
--- Modify Table Structure (ALTER)
-ALTER TABLE books
-  ADD genre VARCHAR2(50);
-
--- Drop a Table (DROP)
--- Let's say you decide the Loans table is redundant and want to remove it :
-  DROP TABLE loans;
